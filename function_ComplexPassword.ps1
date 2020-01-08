19756bbe38df64a57d0b66225d5b79e651cbc5ce
Jack <jacknvll@gmail.com>
2020-01-09 10:24:41 +1100

    Error resolution 1
    Error during Begin, Process, End:
    Begin : The term 'Begin' is not recognized as the name of a cmdlet, function, script file,
    or operable program.
    
    Resolution: Insert the custom function within the Begin block.
    
    Reason: If using Begin/Process/End blocks, there cannot be any other top-level blocks. So custom functions must be inserted into Begin.

--- function_ComplexPassword.ps1
+++ function_ComplexPassword.ps1
@@ -1,6 +1,6 @@
 ﻿function Generate-Complex {
     [CmdletBinding()]
-    [Alias('gco')]   
+    [Alias('gco','genc')]   
     Param
     (
         # Define number of characters to spit out, could use a Switch statement for this one. ParameterSet as well? as it needs minimum 6 characters
@@ -13,19 +13,21 @@
         [int]
         $Count=1
     )
-    function Get-RandChar {
-        #Complex Generator
-        $lowercase = @('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-        $uppercase = @('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-        $special = @('!','@','#','%','*',',','.','?')
-        $numbers = @('0','1','2','3','4','5','6','7','8','9')
-        $characters = @($lowercase,$uppercase,$special,$numbers)
-
-        $RandomSet = Get-Random -InputObject $characters
-        $RandomFromSet = Get-Random -InputObject $RandomSet
-        return $RandomFromSet
-    }
+    
     Begin {
+        function Get-RandChar {
+            #Complex Generator
+            $lowercase = @('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+            $uppercase = @('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+            $special = @('!','@','#','%','*',',','.','?')
+            $numbers = @('0','1','2','3','4','5','6','7','8','9')
+            $characters = @($lowercase,$uppercase,$special,$numbers)
+    
+            $RandomSet = Get-Random -InputObject $characters
+            $RandomFromSet = Get-Random -InputObject $RandomSet
+            return $RandomFromSet
+        }
+
         $Password = @()
         $Loop = @()
         $Error.Clear()
